e7129c4ea886fb31e0cc256ef46ee8158fe19b66
Emaxy10 <emmanuelchigozie01@gmail.com>
2026-09-25 00:03:18 +0100

    Initial Terraform Azure infrastructure

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
