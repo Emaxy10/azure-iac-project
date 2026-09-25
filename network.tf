83051b26cc417176d1d87dd8a792b9293a7c4180
Emaxy10 <emmanuelchigozie01@gmail.com>
2026-09-25 01:04:59 +0100

    Add Azure virtual network and subnets

---
+++ network.tf
@@ -0,0 +1,20 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "azure-iac-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
